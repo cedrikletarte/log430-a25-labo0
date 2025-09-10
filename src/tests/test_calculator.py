08f6dcd82d86fc26bfca2e6515611a1b910bed26
Cedrik Letarte <cedrik.letarte.1@ens.etsmtl.ca>
2025-09-10 19:38:57 -0400

    Tests pour calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -8,6 +8,30 @@ from calculator import Calculator
 
 def test_app():
     my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
+    assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
-# TODO: ajoutez les tests
+def test_addition():
+    my_calculator = Calculator()
+    assert my_calculator.addition(2, 3) == 5
+
+def test_addition_error():
+    my_calculator = Calculator()
+    assert my_calculator.addition(2, 3) == 6  # This test is supposed to fail
+
+def test_subtraction():
+    my_calculator = Calculator()
+    assert my_calculator.subtraction(5, 3) == 2
+
+def test_multiplication():
+    my_calculator = Calculator()
+    assert my_calculator.multiplication(4, 3) == 12
+
+def test_division():
+    my_calculator = Calculator()
+    # normal division
+    assert my_calculator.division(10, 2) == 5.0
+
+    # division by zero
+    result = my_calculator.division(10, 0)
+    assert result == "Erreur : division par zéro"
+    assert my_calculator.last_result == "Error"
